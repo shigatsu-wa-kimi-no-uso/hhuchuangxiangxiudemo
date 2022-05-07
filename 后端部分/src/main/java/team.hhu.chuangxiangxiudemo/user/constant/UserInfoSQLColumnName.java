0f046bff558bffaaf9441a789e6b8c1ce3d414f0
kiminouso <1790226429@qq.com>
2022-05-07 13:05:26 +0000

    用户模块常用常数类---sql列名

---
+++ 后端部分/src/main/java/team.hhu.chuangxiangxiudemo/user/constant/UserInfoSQLColumnName.java
@@ -0,0 +1,19 @@
+package team.hhu.chuangxiangxiudemo.user.constant;
+
+
+
+/**
+ * 用户信息对应sql数据库列名
+ */
+
+
+public final class UserInfoSQLColumnName
+{
+    final public static String USER_NAME="user_name";
+    final public static String USER_EMAIL="user_email";
+    final public static String USER_ID="user_id";
+    final public static String USER_PASSWORD="user_password";
+    final public static String REG_DATE="reg_date";
+
+    UserInfoSQLColumnName(){}
+}
